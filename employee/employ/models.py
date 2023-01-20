4d00e7876e1a392b895503e1d38854a4d9d9dfd4
aiswarya <jayaaishu02@gmail.com>
2023-01-20 11:13:38 +0530

    emp rest

---
+++ employee/employ/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    firstname=models.CharField(max_length=100)
+    lastname=models.CharField(max_length=100)
+    email=models.EmailField()
+    experience=models.IntegerField()
